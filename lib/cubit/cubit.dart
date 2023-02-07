c72d6bae950ea1b9e308c1b1183be936ae130306
MosasaUnited <mostafa447@hotmail.com>
2023-02-08 01:15:33 +0200

    StateManagementStarting

---
+++ lib/cubit/cubit.dart
@@ -0,0 +1,26 @@
+import 'package:basketball_counter_app/cubit/state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<CounterState>
+{
+  CounterCubit(initialState) : super(initialState);
+
+  int teamAPoints = 0;
+
+  int teamBPoints = 0;
+
+  void TeamIncrement({required String team, required int buttonNumber})
+  {
+    if (team == 'A')
+    {
+      teamAPoints += buttonNumber;
+      emit(CounterTeamAState());
+    }else{
+      teamBPoints += buttonNumber;
+      emit(CounterTeamBState());
+    }
+  }
+
+
+
+}
---
+++ lib/cubit/state.dart
@@ -0,0 +1,13 @@
+import 'package:basketball_counter_app/cubit/cubit.dart';
+
+abstract class CounterState {}
+
+class CounterTeamAState extends CounterState
+{
+
+}
+
+class CounterTeamBState extends CounterState
+{
+
+}
